d2ec56343dda60eeeb2bfc7c0f5ac9ce5070bdfd
nagamaninaga <111356935+nagamaninaga@users.noreply.github.com>
2022-08-21 11:18:55 -0400

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,35 @@
+console.log("resume");
+
+let my own Resume 
+
+    "basics": {
+      "name": "NAGAMANI .N",
+      "email": "nagadj007@gmail.com.com",
+      "phone": 6374007723,
+      "degree": "BSC (cs)"
+    },
+      "location": {
+        "address": "1/14 JOTHINAGAR ,aundipatty",
+        "postalCode": 625512,
+        "city": "theni",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+       "education": 
+      {
+        "institution": "senthamarai clg of arts and science",
+        "department": "computer science",
+        "studyType": "fulltime",
+        "batch start year": 2014,
+        "batch end year": 2018,
+      }
+    "languages": 
+      {
+        "language": "Tamil,Enlish,kannada",
+      }
+    "interests": 
+      {
+        "name": "script writter,vollyball,",
+      }
+    
+  
